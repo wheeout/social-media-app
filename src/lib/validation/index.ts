be273d7f830da277d795f0869f42d68192b821d5
Mia Abreu e Lima <xmomoon@gmail.com>
2023-12-24 20:08:59 -0300

    first commit

---
+++ src/lib/validation/index.ts
@@ -0,0 +1,13 @@
+import * as z from "zod";
+
+export const SignupValidation = z.object({
+  name: z.string().min(2, { message: "Too short" }),
+  username: z.string().min(2, { message: "Too short" }),
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Too short" }),
+});
+
+export const SigninValidation = z.object({
+  email: z.string().email(),
+  password: z.string().min(8, { message: "Too short" }),
+});
